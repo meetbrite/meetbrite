7ccc15daf6088d1ff83481d5783c223b66a78156
github username <github email address>
2020-06-12 17:30:01 -0400

    adding views for events

--- db/migrate/20200612194551_create_events.rb
+++ db/migrate/20200612194551_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
       t.boolean :public
-      t.boolean :active
+      t.boolean :active, default: true
       t.string :title
       t.text :description
       t.string :location
